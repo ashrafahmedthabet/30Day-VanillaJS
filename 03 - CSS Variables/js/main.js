306457df00b90acdee12a1f85275dc7b74e27e38
ashrafahmedthabet <asthabet@noon.com>
2023-12-07 12:02:30 +0200

    CSS Variables

---
+++ 03 - CSS Variables/js/main.js
@@ -0,0 +1,11 @@
+let inputs = document.querySelectorAll(".controls input");
+const updateInputValue = (e) => {
+  let element = e.target;
+  let suffix = element.dataset.sizing || "";
+  document.documentElement.style.setProperty(
+    `--${element.name}`,
+    `${element.value}${suffix}`
+  );
+  console.log(suffix);
+};
+inputs.forEach((input) => input.addEventListener("input", updateInputValue));
